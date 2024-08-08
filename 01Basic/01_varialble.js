27795e057aba847340c737e52f93e5c027253b92
ShivamGarg30304 <shivamgarg30304@gmail.com>
2024-08-09 00:11:14 +0530

    basic

---
+++ 01Basic/01_varialble.js
@@ -0,0 +1,7 @@
+const accountId = 144534;
+let accountEmail = "shivamgarg30304@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+accountEmail = 5;
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01Basic/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("Shivam Garg");
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined => not assign a value
+// symbol => unique k liye use krte hai
+
+// object
+
+console.log(typeof "shivam");
+
+console.log(typeof undefined); // undefined;
+
+console.log(typeof null); // object
