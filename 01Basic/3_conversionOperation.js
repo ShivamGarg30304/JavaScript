f5e87a4f15d73726a82b5f9dea930ee96c0505c1
ShivamGarg30304 <shivamgarg30304@gmail.com>
2024-08-09 16:25:06 +0530

    feat: Add code for converting string to number and performing comparison operations
    This commit adds code for converting a string to a number using the Number() function and performing comparison operations. It also includes examples of different conversion scenarios and strict equality checks.

---
+++ 01Basic/3_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "33abc";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+// let sNum = -33;
+let sNum = undefined;
+// console.log(String(sNum) + "1");
+// console.log(Number("02"));
+
+// operations
+
+// console.log("2" > 1);
+// console.log("-1" > 1);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+// strict check
+console.log("2" == 2); // true
+console.log("2" === 2); // false
+
+console.log("2" != 2); // false
+console.log("2" !== "2"); // true
---
+++ 01Basic/4_dataTypes.summary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+// console.log(id === anotherId);
+// console.log(id);
+// console.log(anotherId);
+
+const bigNumber = 3424567654567654544454545454845851n;
+console.log(typeof bigNumber);
+
+// Non -Primitive or Refrenced Type
+
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "doga", "naagraj"];
+
+const person = {
+  name: "shivam",
+  age: 20,
+};
+
+const myFunction = function () {
+  console.log("Hello world");
+};
+
+console.log(typeof myFunction);
